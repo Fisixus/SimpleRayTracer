fd1c87ce2cf2ee9e52e9d2df608e7ba7264add05
Fisixus <31778258+Fisixus@users.noreply.github.com>
2020-03-19 17:55:03 +0300

    Configuration files has been implemented.

---
+++ RayTracer.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include "Angel.h"
+#include <vector>
+#include <Math.h>
+
+using namespace Angel;
+using namespace std;
+
+typedef vec4 point4;
+typedef vec4 color4;
+
+int main(int argc, char **argv) {
+	printf("Hello\n");
+}
